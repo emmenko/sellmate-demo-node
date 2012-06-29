0892bdd1b1f6738176218e1ee9ff5e404433e471
Nicola Molinari <nicola.molinari@commercetools.de>
2012-06-29 17:49:17 +0200

    removed properties.js

--- properties.js
+++
@@ -1,6 +0,0 @@
-// Sellmate application settings
-exports.settings = {
-    client_id: '', // Your application client id
-    client_secret: '', // Your application client secret
-    redirect_uri: '' // Your application redirect URI (callback)
-}
