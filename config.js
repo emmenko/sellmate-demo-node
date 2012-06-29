ee4e57dd3660a603ac1166c6100a8c945e789de6
Nicola Molinari <nicola.molinari@commercetools.de>
2012-06-29 17:42:04 +0200

    Added properties file to store the app settings

--- config.js
+++ config.js
@@ -1,3 +1,5 @@
+var settings = require('./properties.js').settings;
+
 //exports.host_api: 'http://api.sellmate.com';
 //exports.host_auth: 'https://auth.sellmate.com';
 exports.host_api = 'api.sellmatepages.com';
@@ -7,11 +9,10 @@ exports.response_type = 'code';
 exports.grant_type_auth = 'authorization_code';
 exports.grant_type_refresh = 'refresh_token';
 exports.shop = 'test';
-exports.client_id = 12002;
-exports.client_secret = 'GDqbHdpkm4kbJ7loT';
-exports.redirect_uri = 'http://localhost:8888/callback';
-exports.charset = 'UTF-8';
-exports.mac_algorithm = 'HmacSHA256';
+exports.client_id = settings.client_id;
+
+exports.client_secret = settings.client_secret;
+exports.redirect_uri = settings.redirect_uri;
 
 exports.pageLimit = 25;
 
