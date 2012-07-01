1e5db4982bf73d15efdc6778c1571e2f1c937432
Nicola Molinari <nicola.molinari@commercetools.de>
2012-07-01 12:07:30 +0200

    Minor change

--- properties.js
+++ properties.js
@@ -2,5 +2,5 @@
 exports.settings = {
     client_id: '12002', // Your application client id
     client_secret: 'GDqbHdpkm4kbJ7loT', // Your application client secret
-    redirect_uri: 'http://sellmate-demo.nodester.com/callback' // Your application redirect URI (callback)
+    redirect_uri: 'http://localhost:8888/callback' // Your application redirect URI (callback)
 }
